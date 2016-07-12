3415a8987cda0638c69eb5c08f6a8ffcbb3f01e7
Jacky1100 <609615907@qq.com>
2016-07-12 11:49:28 +0800

    Jack update

--- src/main/webapp/resources/js/portal.js
+++ src/main/webapp/resources/js/portal.js
@@ -59,7 +59,7 @@ $().ready(function(){
 	// 每2分钟检测 订单状态，如果有 新订单 则弹出提示
 	/*checkIndentStatus();*/
 	//每2分钟检测 用户评级，如果有新用户未评级 则弹出提示
-	/*checkUsrClientLevel();*/
+	checkUsrClientLevel();
 	
 });
 
@@ -143,7 +143,7 @@ function checkUsrClientLevel(){
 				var li_list = $('#menu').find('.tree-title');
 				
 				$.each(li_list,function(i,n){
-					if($(n).text() == '客户管理'){
+					if($(n).text() == '客户列表管理'){
 						$(this).find('.badge').remove();
 						var $span = '<span class="badge">new-';
 						$span += count;
