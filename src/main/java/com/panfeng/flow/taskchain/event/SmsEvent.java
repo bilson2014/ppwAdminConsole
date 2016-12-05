d86a257b16ac682f720f16b84a678bcd2c7786e2
laowang <1300792971@qq.com>
2016-12-05 10:24:33 +0800

    lw

--- src/main/java/com/panfeng/flow/taskchain/event/SmsEvent.java
+++ src/main/java/com/panfeng/flow/taskchain/event/SmsEvent.java
@@ -76,7 +76,7 @@ public class SmsEvent extends EventBase {
 				fillerParam.setRelevantPersons(relevantPersonsList);
 			}
 			Object fillData = templateDataManage.fillData(fillerParam, autoEvent.getDataFiller(), processId,
-					EventType.MAIL);
+					EventType.SMS);
 			if (fillData != null) {
 				Sms findSmsById = smsTemplateService.findSmsById(Long.parseLong(autoEvent.getTemplateId()));
 				String content = findSmsById.getTempContent();
