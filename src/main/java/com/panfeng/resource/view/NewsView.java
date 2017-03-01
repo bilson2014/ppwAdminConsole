2f520e6f71c819802bf90577d2e30bc581584390
laowang <1300792971@qq.com>
2017-03-01 18:44:04 +0800

    lw

---
+++ src/main/java/com/panfeng/resource/view/NewsView.java
@@ -0,0 +1,17 @@
+package com.panfeng.resource.view;
+
+public class NewsView extends Pagination {
+
+	private static final long serialVersionUID = 4923161285696056L;
+
+	private Integer category;
+
+	public Integer getCategory() {
+		return category;
+	}
+
+	public void setCategory(Integer category) {
+		this.category = category;
+	}
+
+}
