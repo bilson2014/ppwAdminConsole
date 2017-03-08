60d6be17e8543db512db6fddb056c8214bd12a7e
laowang <1300792971@qq.com>
2017-03-08 16:11:02 +0800

    lw

--- src/main/java/com/panfeng/service/impl/NewsServiceImpl.java
+++ src/main/java/com/panfeng/service/impl/NewsServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.graphbuilder.struc.LinkedList;
 import com.panfeng.persist.NewsMapper;
 import com.panfeng.resource.model.News;
 import com.panfeng.resource.view.NewsView;
